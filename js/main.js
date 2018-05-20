23b700e9df5d068b1d193c3e541641c329b3e985
Koji Yamamoto <kyamuise@gmail.com>
2018-05-20 18:07:31 +0900

    0.3.0

--- js/main.js
+++ js/main.js
@@ -1,34 +1,60 @@
+'use strict'
+
 function appendNodeHeader(node) {
-    $("#bookmarks").append(`
-        <div class="pure-u-1 pure-u-sm-1-2 pure-u-md-1-3 pure-u-lg-1-6  list-item-wrapper" id="category-${node.title}-wrapper">
-            <div class="bookmark-folder">
-                <span class="pure-menu-heading category-header">${node.title}</span>
-                <div class="pure-menu pure-menu-scrollable bookmark-items">
-                    <ul class="pure-menu-list" id="category-${node.title}"></ul>
-                </div>
-            </div>
-        </div>
-        `);
+    const elem = document.createElement("div");
+    elem.setAttribute("id", "category-" + node.title+"-wrapper");
+    elem.setAttribute("class", "pure-u-1 pure-u-sm-1-2 pure-u-md-1-3 pure-u-lg-1-6 list-item-wrapper");
+
+    console.log(elem);
+
+    const elem2 = document.createElement("div");
+    elem2.setAttribute("class", "bookmark-folder");
+
+    const elem3 = document.createElement("span");
+    elem3.setAttribute("class", "pure-menu-heading category-header");
+    elem3.innerText = node.title;
+
+    const elem4 = document.createElement("div");
+    elem4.setAttribute("class", "pure-menu pure-menu-scrollable bookmark-items");
+
+    const elem5 = document.createElement("ul");
+    elem5.setAttribute("class", "pure-menu-list");
+    elem5.setAttribute("id", "category-" + node.title);
+
+    elem4.append(elem5);
+    elem2.append(elem3, elem4);
+    elem.append(elem2);
+
+    document.querySelector("#bookmarks").append(elem);
 }
 
 function deleteNodeHeader(node) {
-    $(`#category-${node.title}-wrapper`).remove();
+    document.querySelector("#category-" + node.title+"-wrapper").remove();
 }
 
 function appendNodeItem(node, item) {
     const faviconUrl = "https://www.google.com/s2/favicons?domain_url=" + item.url;
-    $(`#category-${node.title}`).append(`
-        <li class="pure-menu-item" title="${item.title}"><a href="${item.url}" class="pure-menu-link bookmark-item">
-            <img src="${faviconUrl}" class="bookmark-item-favicon"/>
-                ${item.title}
-            </a>
-        </li>
-    `);
+
+    const elem = document.createElement("li");
+    elem.setAttribute = ("class", "pure-menu-item");
+    elem.setAttribute = ("title", item.title);
+
+    const elem2 = document.createElement("a");
+    elem2.setAttribute("href", item.url);
+    elem2.setAttribute("class", "pure-menu-link bookmark-item");
+
+    const elem3 = document.createElement("img");
+    elem3.setAttribute("src", faviconUrl);
+    elem3.setAttribute("class", "bookmark-item-favicon");
+
+    elem2.append(elem3, " " + item.title);
+    elem.append(elem2);
+
+    document.querySelector("#category-" + node.title).appendChild(elem);
 }
 
 function loadSubTree(node) {
     let itemCount = 0;
-    console.log(node);
     appendNodeHeader(node);
     node.children.forEach(item => {
     if (item.type == "bookmark" && !item.url.match(/^place:.*$/)) {
